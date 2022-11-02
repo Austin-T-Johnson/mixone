eae2617eb04e5c5f25259192f25aff0e038c6ab2
Austin Johnson <austintjohnson92@gmail.com>
2022-11-02 16:01:07 -0700

    services section

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,18 +1,42 @@
 import React from 'react'
 import img from '../assets/images/David-Guetta.jpeg'
+import services from '../JSON/services.json'
+
 const Landing = () => {
-  return (
-    <div className='top-container'>
-        <div className='img-div'>
-            <img src={img} alt="David Guetta On Stage" className='david-guetta-img'></img>
+    return (
+        <div className='top-container'>
+            <div className='img-div'>
+                <img src={img} alt="David Guetta On Stage" className='david-guetta-img'></img>
+            </div>
+            <div className='span-div'>
+                <span className='mix1-span'>MixOne Sound</span>
+            </div>
+            <div className='p-div'>
+                <p>Southern Californias Premiere Live Event Production Company</p>
+            </div>
+
+            <div className='lower-container'>
+
+                <div className='services-h1'>
+                    Full-Service Event Production Solution
+                </div>
+
+                <div className='services-container'>
+                    {services.map((service) => {
+                        return (
+                            <div className='services-inner-container' key={service.id}>
+                                <div className='services-txt-div'>
+                                    <h3 className='services-name'>{service.name}</h3>
+                                    <span className='services-txt'>{service.txt}</span>
+                                </div>
+                                <img className='services-img' src={service.img} alt="services we provide"></img>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
         </div>
-        <div className='span-div'>
-             <span>MixOne Sound</span> 
-             <p>Southern Californias Premiere Live Event Production Company</p>
-        </div>
-       
-    </div>
-  )
+    )
 }
 
 export default Landing
